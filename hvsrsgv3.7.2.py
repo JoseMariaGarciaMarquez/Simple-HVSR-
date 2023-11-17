f641e915dd667b80c44f94521022874b0f1ab74d
José María García Márquez <30852961+JoseMariaGarciaMarquez@users.noreply.github.com>
2023-11-17 16:10:32 -0600

    hvsrg4.0.py

--- hvsrsgv3.7.2.py
+++ hvsrsgv3.7.2.py
@@ -1,34 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
 import psutil
 import numpy as np
 from obspy import read
 from scipy import signal
-import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from obspy.signal.invsim import corn_freq_2_paz
 
-
-def plotear(z, n, e):
+def browse_file(entry_var):
     """
-    Plot the seismic data components Z, N, E.
+    Browse files function
     """
-    paz_sts2 = {
-    'poles': [-62.382 +135.39j, -62.382 - 135.39j, -350.00 + 0j,
-               -75.000 - 0j, -23.560e-3 + 23.560e-3j, -23.560e-3 + 23.560e-3j],
-    'zeros': [0 + 0j, 0 + 0j],
-    'gain': 60077000.0,
-    'sensitivity': 5.858e+8}
-    paz_1hz = corn_freq_2_paz(1.0, damp=0.707) 
-    paz_1hz['sensitivity'] = 1.0
+    file_path = filedialog.askopenfilename(filetypes=[('SAC Files', '*.sac')])
+    entry_var.set(file_path)
 
-    
+
+def plotear(z, n, e, ventana, ventaneo, overlap):
+
+
+    ventana = int(ventana)
+    overlap = int(overlap)
+
     # Cargar datos con ObsPy
-    st_z = obspy.read(z)
-    st_n = obspy.read(n)
-    st_e = obspy.read(e)
+    st_z = read(z)
+    st_n = read(n)
+    st_e = read(e)
     
-    st_z = st_z.simulate(paz_remove = paz_sts2, paz_simulate = paz_1hz)
-    st_n = st_n.simulate(paz_remove = paz_sts2, paz_simulate = paz_1hz)
-    st_e = st_e.simulate(paz_remove = paz_sts2, paz_simulate = paz_1hz)
+
 
     # Obtener arreglos NumPy de los datos sísmicos
     z = st_z[0].data
@@ -48,7 +46,7 @@ def plotear(z, n, e):
     fz, Pz = signal.welch(z, fs=st_z[0].stats.sampling_rate, window=ventaneo, nperseg=ventana, noverlap=overlapping, scaling='spectrum')
     fn, Pn = signal.welch(n, fs=st_n[0].stats.sampling_rate, window=ventaneo, nperseg=ventana, noverlap=overlapping, scaling='spectrum')
     fe, Pe = signal.welch(e, fs=st_e[0].stats.sampling_rate, window=ventaneo, nperseg=ventana, noverlap=overlapping, scaling='spectrum')
-    
+        
     fig, ax = plt.subplots(3, 2, figsize=(15, 10), dpi=200)
     ax[0, 0].plot(st_z[0].times("matplotlib"), z)
     ax[0, 1].plot(fz, Pz)
@@ -66,23 +64,22 @@ def plotear(z, n, e):
 
 def calculate_hvsr(z, n, e, ventana, ventaneo, overlap):
     """
-    Calculate the Horizontal-to-Vertical Spectral Ratio (HVSR) for seismic data.
+    Calcula la relación espectral horizontal a vertical (HVSR) para datos sísmicos.
+    
     Args:
-        z (str): Path to the Z component data file.
-        n (str): Path to the N component data file.
-        e (str): Path to the E component data file.
-        ventana (int): Number of samples in the window.
-        ventaneo (str): Type of window.
-        overlap (float): Overlapping percentage.
+        z (str): Ruta al archivo de datos de la componente Z.
+        n (str): Ruta al archivo de datos de la componente N.
+        e (str): Ruta al archivo de datos de la componente E.
+        ventana (int): Número de muestras en la ventana.
+        ventaneo (str): Tipo de ventana.
+        overlap (float): Porcentaje de solapamiento.
+        
+    Returns:
+        float: Frecuencia del sitio calculada.
     """
-    paz_sts2 = {
-    'poles': [-62.382 +135.39j, -62.382 - 135.39j, -350.00 + 0j,
-               -75.000 - 0j, -23.560e-3 + 23.560e-3j, -23.560e-3 + 23.560e-3j],
-    'zeros': [0 + 0j, 0 + 0j],
-    'gain': 60077000.0,
-    'sensitivity': 5.858e+8}
-    paz_1hz = corn_freq_2_paz(1.0, damp=0.707) 
-    paz_1hz['sensitivity'] = 1.0
+    ventana = int(ventana)
+    overlap = int(overlap)
+
 
     # Cargar datos con ObsPy
     st_z = read(z)
@@ -91,9 +88,7 @@ def calculate_hvsr(z, n, e, ventana, ventaneo, overlap):
 
     ventana_segs = ventana / st_z[0].stats.sampling_rate
 
-    st_z = st_z.simulate(paz_remove = paz_sts2, paz_simulate = paz_1hz)
-    st_n = st_n.simulate(paz_remove = paz_sts2, paz_simulate = paz_1hz)
-    st_e = st_e.simulate(paz_remove = paz_sts2, paz_simulate = paz_1hz)
+
 
     # Obtener arreglos NumPy de los datos sísmicos
     z = st_z[0].data
@@ -101,7 +96,7 @@ def calculate_hvsr(z, n, e, ventana, ventaneo, overlap):
     e = st_e[0].data
 
     # Overlapping calculation
-    overlapping = (overlap/100) * ventana
+    overlapping = (overlap / 100) * ventana
 
     # Linear detrend the data
     z = signal.detrend(z, type='linear')
@@ -114,18 +109,17 @@ def calculate_hvsr(z, n, e, ventana, ventaneo, overlap):
     fe, Pe = signal.welch(e, fs=st_e[0].stats.sampling_rate, window=ventaneo, nperseg=ventana, noverlap=overlapping, scaling='spectrum')
 
     # Calculate HVSR
-    Hn = Pn / Pz
-    He = Pe / Pz
-    HVSRPico = (np.sqrt(Pn * Pe)) / Pz
-    HVSRLuMa = np.sqrt((Pn + Pe) / Pz)
-    Nakamura = (np.sqrt(Pn**2 + Pe**2)) / Pz
+    Hn = Pn / Pz  # Lunedei and Albarello N
+    He = Pe / Pz  # Lunedei and Albarello E
+    HVSRPico = (np.sqrt(Pn * Pe)) / Pz  # Picozzi
+    HVSRLuMa = np.sqrt((Pn + Pe) / Pz)  # Lunedei and Malischewsky
+    Nakamura = (np.sqrt(Pn**2 + Pe**2)) / Pz  # Nakamura
 
     f = (fn + fe) / 2
 
     # Filtrar frecuencias mayores a 0.3 Hz
     mask = f >= 0.3
     f_filtered = f[mask]
-
 
     HVSRPico_filtered = HVSRPico[mask]
     HVSRLuMa_filtered = HVSRLuMa[mask]
@@ -143,8 +137,8 @@ def calculate_hvsr(z, n, e, ventana, ventaneo, overlap):
     title = input('Título de la gráfica\n')
     fig, ax = plt.subplots(figsize=(7, 5), dpi=300)
     ax.axvline(frecuencia_sitio, c='lightgrey', linewidth=10)
-    ax.plot(fn, Hn, label = 'Lunedei and Albarello N')
-    ax.plot(fe, He, label = 'Lunedei and Albarello E')
+    ax.plot(fn, Hn, label='Lunedei and Albarello N')
+    ax.plot(fe, He, label='Lunedei and Albarello E')
     ax.plot(f_filtered, HVSRPico_filtered, label='Picozzi')
     ax.plot(f_filtered, HVSRLuMa_filtered, label='Lunedei and Malischewsky')
     ax.plot(f_filtered, Nakamura_filtered, label='Nakamura')
@@ -163,21 +157,22 @@ def calculate_hvsr(z, n, e, ventana, ventaneo, overlap):
 
     # Save the results to a text file
     with open('resultados_{}.txt'.format(nombre), 'w') as file:
-     file.write('------------------------------------------🥑RESULTADOS🥑------------------------------------------\n'
-                       'Datos:\n{}\n{}\n{}\n'
-                       '-------------------------------------------------------------------------------------------------\n'
-                       'Con un ventaneo de {}s que asegura {} muestras\n'
-                       'Y el tipo de ventana usada de {}\n'
-                       'Con un solapamiento del {}%\n'
-                       'La frecuencia del sitio es {}[Hz]\n'
-                       'El periodo del sitio es {}[m]\n'
-                       'El cálculo de la velocidad de corte es {}[m/s]\n'
-                       'El espesor de la capa de sedimento es de {}[m]\n'
-                       '-------------------------------------------------------------------------------------------------\n'
-                       .format(st_z[0].stats, st_n[0].stats, st_e[0].stats, ventana_segs,ventana, ventaneo, overlap, frecuencia_sitio,periodo_sitio, vs, h))
+        file.write('------------------------------------------🥑RESULTADOS🥑------------------------------------------\n'
+                   'Datos:\n{}\n{}\n{}\n'
+                   '-------------------------------------------------------------------------------------------------\n'
+                   'Con un ventaneo de {}s que asegura {} muestras\n'
+                   'Y el tipo de ventana usada de {}\n'
+                   'Con un solapamiento del {}%\n'
+                   'La frecuencia del sitio es {}[Hz]\n'
+                   'El periodo del sitio es {}[m]\n'
+                   'El cálculo de la velocidad de corte es {}[m/s]\n'
+                   'El espesor de la capa de sedimento es de {}[m]\n'
+                   '-------------------------------------------------------------------------------------------------\n'
+                   .format(st_z[0].stats, st_n[0].stats, st_e[0].stats, ventana_segs, ventana, ventaneo, overlap,
+                           frecuencia_sitio, periodo_sitio, vs, h))
 
     proceso_actual = psutil.Process()
-    uso_de_memoria = proceso_actual.memory_info().rss 
+    uso_de_memoria = proceso_actual.memory_info().rss
     uso_de_memoria_mb = uso_de_memoria / (1024 * 1024)
     print(f"Uso de memoria actual: {uso_de_memoria_mb:.2f} MB")
 
@@ -185,39 +180,47 @@ def calculate_hvsr(z, n, e, ventana, ventaneo, overlap):
 
     return frecuencia_sitio
 
+
 def main_window():
     window_options = ['boxcar', 'triang', 'blackman', 'hamming', 'hann', 'bartlett', 'flattop', 'parzen', 'bohmann', 'blackmanharris', 'nuttall', 'barthan', 'cosine', 'exponential', 'tukey', 'taylor', 'lanczos']
     ventana_size = [1024, 2048, 4096, 8202]
-    overlap = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    overlap_values = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+
+    window = tk.Tk()
+    window.title('Simple-HVSR(中村)')
+
+    # Define a Tkinter StringVar for each Entry
+    sacz_var = tk.StringVar()
+    sacn_var = tk.StringVar()
+    sace_var = tk.StringVar()
 
     # Define the layout
     layout = [
-        [sg.Text('---------------------------------------------------Cargar componentes---------------------------------------------------')],
-        [sg.Text('Componente Z'), sg.Input(key='-SACZ-'), sg.FileBrowse()],
-        [sg.Text('Componente N'), sg.Input(key='-SACN-'), sg.FileBrowse()],
-        [sg.Text('Componente E'), sg.Input(key='-SACE-'), sg.FileBrowse()],
-        [sg.Text('-----------------------------------------------------Procesamiento-----------------------------------------------------')],
-        [sg.Text('Muestreo'), sg.Combo(ventana_size, key='-VENTANA-'),sg.Text('Ventaneo'), sg.Combo(window_options, key='-WINDOW-'), sg.Text('Overlap by'), sg.Combo(overlap, key='OLAP'), sg.Text('%')],
-        [sg.Text('---------------------------------------------------------------------------------------------------------------------------------')],
-        [sg.Button('Calculate HVSR'), sg.Button('Visualize data')],
-        [sg.Button("Exit")],
+        [tk.Label(text='Cargar componentes', font=('Helvetica', 14, 'bold'))],
+        [tk.Label(text='Componente Z'), tk.Entry(textvariable=sacz_var), tk.Button(text='Browse', command=lambda: browse_file(sacz_var))],
+        [tk.Label(text='Componente N'), tk.Entry(textvariable=sacn_var), tk.Button(text='Browse', command=lambda: browse_file(sacn_var))],
+        [tk.Label(text='Componente E'), tk.Entry(textvariable=sace_var), tk.Button(text='Browse', command=lambda: browse_file(sace_var))],
+        [tk.Label(text='Procesamiento',font=('Helvetica', 14, 'bold'))],
+        [tk.Label(text='Muestreo'), ttk.Combobox(values=ventana_size)],
+        [tk.Label(text='Ventaneo'), ttk.Combobox(values=window_options)],
+        [tk.Label(text='Overlap by'), ttk.Combobox(values=overlap_values), tk.Label(text='%')],
+        [tk.Button(text='Calculate HVSR', command=lambda: calculate_hvsr(sacz_var.get(), sacn_var.get(), sace_var.get(), ventana_combobox.get(), ventaneo_combobox.get(), overlap_combobox.get()))],
+        [tk.Button(text='Visualize data', command=lambda: plotear(sacz_var.get(), sacn_var.get(), sace_var.get(), ventana_combobox.get(), ventaneo_combobox.get(), overlap_combobox.get()))],
+        [tk.Button(text="Exit", command=lambda: exit())],
     ]
 
-    window = sg.Window('Simple-HVSR(中村)', layout)
+    # Populate the layout
+    for row in layout:
+        if row:
+            for idx in range(len(row)):
+                row[idx].grid(row=layout.index(row), column=idx, padx=5, pady=5)
 
-    # While the window is open
-    while True:
-        event, values = window.read()
-        if event in (sg.WINDOW_CLOSED, 'Exit'):
-            break
+    ventana_combobox = layout[5][1]
+    ventaneo_combobox = layout[6][1]
+    overlap_combobox = layout[7][1]
 
-        if event == 'Calculate HVSR':
-            calculate_hvsr(values['-SACZ-'], values['-SACN-'], values['-SACE-'], values['-VENTANA-'], values['-WINDOW-'], values['OLAP'])
-
-        if event == 'Visualize data':
-            plotear(values['-SACZ-'], values['-SACN-'], values['-SACE-'])
-
-    window.close()
+    # Run the main loop
+    window.mainloop()
 
 if __name__ == '__main__':
     main_window()
